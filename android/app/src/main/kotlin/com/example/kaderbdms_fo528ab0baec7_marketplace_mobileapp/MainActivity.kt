c6244c0f00bca0a7aa5fb427a8124a6c2b445c9a
Arif <mdarifhosain214@gmail.com>
2025-11-24 12:42:17 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/kaderbdms_fo528ab0baec7_marketplace_mobileapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kaderbdms_fo528ab0baec7_marketplace_mobileapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'core/route/route_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Riverpod MVVM Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
+    );
+  }
+}
